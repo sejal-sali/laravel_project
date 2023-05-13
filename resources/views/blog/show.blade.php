bda5498f13ddbad5220a7e69144e16071f9b30f2
Sejal Sali <sejalsali070402@gmail.com>
2023-05-13 16:45:32 +0100

    post comment form

--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -41,7 +41,70 @@
 @endforeach
 @endif
 
+<main class="sm:container sm:mx-auto sm:max-w-lg sm:mt-10 w-1/2">
+    <div class="flex">
+        <div class="w-full">
+            <section class="flex flex-col break-words bg-white sm:border-1 sm:rounded-md sm:shadow-sm sm:shadow-lg">
 
+                <header class="font-semibold bg-gray-200 text-gray-700 py-8 px-12 sm:py-6 sm:px-8 sm:rounded-t-md">
+                    {{ __('Post Comment') }}
+                </header>
+
+                <form class="w-full px-6 space-y-6 sm:px-10 sm:space-y-8" method="POST" action="{{ route('comment') }}">
+                    @csrf
+
+                    <input type="hidden" name="blog_id" value="{{$post->id}}"/>
+
+                    <div class="flex flex-wrap">
+                        <label for="name" class="block text-gray-700 text-sm font-bold mb-2 sm:mb-4">
+                            {{ __('Name') }}:
+                        </label>
+
+                        <input id="name" type="text" class="form-input w-full @error('name')  border-red-500 @enderror"
+                            name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+
+                        @error('name')
+                        <p class="text-red-500 text-xs italic mt-4">
+                            {{ $message }}
+                        </p>
+                        @enderror
+                    </div>
+
+                    <div class="flex flex-wrap">
+                        <label for="email" class="block text-gray-700 text-sm font-bold mb-2 sm:mb-4">
+                            {{ __('E-Mail Address') }}:
+                        </label>
+
+                        <input id="email" type="email"
+                            class="form-input w-full @error('email') border-red-500 @enderror" name="email"
+                            value="{{ old('email') }}" required autocomplete="email" autofocus>
+
+                        @error('email')
+                        <p class="text-red-500 text-xs italic mt-4">
+                            {{ $message }}
+                        </p>
+                        @enderror
+                    </div>
+
+                    <textarea
+                    name="comment_text"
+                    placeholder="Comment..."
+                    class="py-20 bg-transparent block border-2 w-full h-40 text-xl outline-none"></textarea>
+
+
+                    <div class="flex flex-wrap">
+                        <button type="submit"
+                        class="w-full select-none font-bold whitespace-no-wrap p-3 rounded-lg text-base leading-normal no-underline text-gray-100 bg-blue-500 hover:bg-blue-700 sm:py-4">
+                            {{ __('Submit') }}
+                        </button>
+
+                    </div>
+                </form>
+
+            </section>
+        </div>
+    </div>
+</main>
 
 
 @endsection 
