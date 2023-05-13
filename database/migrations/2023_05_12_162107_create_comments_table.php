67fd55850c2d38a93f4d86f1ea544a962f1ce204
Sejal Sali <sejalsali070402@gmail.com>
2023-05-13 16:27:47 +0100

    comment table created using migration

---
+++ database/migrations/2023_05_12_162107_create_comments_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name',100);
+            $table->string('email',100);
+            $table->longText('comment_text');
+            $table->timestamps();
+            $table->integer('blog_id')->unsigned()->index();
+            $table->foreign('blog_id')->references('id')->on('posts');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
