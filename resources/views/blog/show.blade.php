bf7ff4610eb3938cb31316a70e7c208d18de0d17
Sejal Sali <sejalsali070402@gmail.com>
2023-05-13 16:44:21 +0100

    display blog comment

--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -19,4 +19,29 @@
     </p>
 </div>
 
+@if($post->comment!=null)
+@foreach ($post->comment as $comment)
+
+
+<div class="sm:grid grid-cols-1 gap-10 w-4/5 mx-auto py-15 border-b border-gray-200 bg-white pl-5">
+    <div class="flex items-center space-x-4">
+        <img class="w-10 h-10 rounded-full" src="{{ asset('images/user.png') }}" alt="">
+        <div class="font-medium dark:text-white">
+            <div> {{ $comment->name }}</div>
+            <div class="text-sm text-gray-500 dark:text-gray-400">Comment on {{ date('jS M Y', strtotime($comment->updated_at)) }}</div>
+        </div>
+    </div>
+
+    <p class="text-xl text-gray-700 pt-1 pb-1 leading-8 font-light">
+        {{ $comment->comment_text }}
+    </p>
+
+
+</div>
+@endforeach
+@endif
+
+
+
+
 @endsection 
